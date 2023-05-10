8eca4134e32b8df26cc44e5b897a6734ef5ae79d
dondoncorre22 <108739929+dondoncorre@users.noreply.github.com>
2023-05-10 17:22:58 +0100

    having fun?

--- sample_code.sh
+++ sample_code.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Hello Don, welcome to DevOps!"
+echo "Are you having fun?"
+
 echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
